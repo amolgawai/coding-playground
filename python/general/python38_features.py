f4e3615d5fabca8dcc6fd52ee85e65c3ac4e5051
Amol Gawai <Amol.Gawai@doesnot.exist>
2020-05-12 17:56:58 +0200

    added Precise type example, elpy tools and mypy
    elpy tools - rope, flymake, yapf
    type checker - mypy

--- python/general/python38_features.py
+++ python/general/python38_features.py
@@ -3,11 +3,13 @@ Overview of features introduced in python 3.8
 ref - https://realpython.com/lessons/cool-new-features-python-38-overview/
 """
 
+from typing import Literal
+
 # The walrus operator - assignment expressions
 # := -> Allows assignment and return of value in same expression
 # more examples - https://www.python.org/dev/peps/pep-0572/#examples
 print(walrus := True)
-print( type(walrus) )
+print(type(walrus))
 
 inputs = list()
 while (current := input('Write something, type quit to quit - ')) != 'quit':
@@ -34,13 +36,33 @@ def headline(text, /, border="~", *, width=50):
     return f" {text} ".center(width, border)
 
 # Since text is positional-only, you can’t use the keyword text:
-print( headline("Positional-only Arguments") )
+print(headline("Positional-only Arguments"))
 # headline(text="This doesn't work!")
 
 # border, on the other hand, can be specified both with and without the keyword:
-print( headline("Python 3.8", "=") )
-print( headline("Real Python", border=":") )
+print(headline("Python 3.8", "="))
+print(headline("Real Python", border=":"))
 
 # Finally, width must be specified using the keyword:
-print( headline("Python", "@", width=38) )
+print(headline("Python", "@", width=38))
 # headline("Python", "@", 38) - This doesn't work
+
+# More precise types
+# type checking as introduced in 3.5
+def double_it(number: float) -> float:
+	return 2*number
+# check with mypy on cli
+double_it(10.5) # passes mypy
+double_it("type check failed") # mypy error
+# 3.8 introduced 'Literal' type which restricts possible values of an argument
+# or return
+def draw_line(direction: Literal["horizontal", "vertical"]) -> None:
+	if direction == "horizontal":
+		print("Drawing horizontal line")
+	elif direction == "vertical":
+		print("Drawing vertical line")
+	else:
+		raise ValueError(f"Invalid direction {direction!r}")
+
+# following shall produce error with mypy
+draw_line("up")
