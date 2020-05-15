48f586bdbd4841fd97f3dfa7f2a7d3481c46698b
Amol Gawai <Amol.Gawai@doesnot.exist>
2020-05-15 15:18:38 +0200

    try expect for Literal example, formatting

--- python/general/python38_features.py
+++ python/general/python38_features.py
@@ -12,15 +12,19 @@ print(walrus := True)
 print(type(walrus))
 
 inputs = list()
-while (current := input('Write something, type quit to quit - ')) != 'quit':
+while (current := input("Write something, type quit to quit - ")) != "quit":
     inputs.append(current)
 print(f"you said {inputs}")
 
 # positional only arguments
-def greet(name, /, greeting = 'Hello'): # the '/' separates positional only and named args
+def greet(
+    name, /, greeting="Hello"
+):  # the '/' separates positional only and named args
     print(f"{greeting} {name}")
+
+
 greet("Jon")
-greet("Jon", greeting = "Hi")
+greet("Jon", greeting="Hi")
 # greet(name = "Jon") - does not work
 # keyword only arguments are available in python 3 which can be separated by '*'
 def headline(text, /, border="~", *, width=50):
@@ -34,6 +38,7 @@ def headline(text, /, border="~", *, width=50):
 
     """
     return f" {text} ".center(width, border)
+
 
 # Since text is positional-only, you can’t use the keyword text:
 print(headline("Positional-only Arguments"))
@@ -50,19 +55,27 @@ print(headline("Python", "@", width=38))
 # More precise types
 # type checking as introduced in 3.5
 def double_it(number: float) -> float:
-	return 2*number
+    return 2 * number
+
+
 # check with mypy on cli
-double_it(10.5) # passes mypy
-double_it("type check failed") # mypy error
+double_it(10.5)  # passes mypy
+double_it("type check failed")  # mypy error
 # 3.8 introduced 'Literal' type which restricts possible values of an argument
 # or return
 def draw_line(direction: Literal["horizontal", "vertical"]) -> None:
-	if direction == "horizontal":
-		print("Drawing horizontal line")
-	elif direction == "vertical":
-		print("Drawing vertical line")
-	else:
-		raise ValueError(f"Invalid direction {direction!r}")
+    if direction == "horizontal":
+        print("Drawing horizontal line")
+    elif direction == "vertical":
+        print("Drawing vertical line")
+    else:
+        raise ValueError(f"Invalid direction {direction!r}")
+
 
 # following shall produce error with mypy
-draw_line("up")
+try:
+    draw_line("up")
+except ValueError:
+    print("Received Value Error Exception")
+
+draw_line("horizontal")
