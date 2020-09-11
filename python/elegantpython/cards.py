482bfd8cb9c55ff1212a7c4737b4684b3b42cc1e
Amol <amolgawai@users.noreply.github.com>
2020-09-11 20:33:05 +0200

    formatting
    check again in the history as this file might have different formatting
    on iPad

--- python/elegantpython/cards.py
+++ python/elegantpython/cards.py
@@ -1,16 +1,17 @@
 import collections
+
 # from random import choice
 import random
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits
-                                        for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
@@ -19,18 +20,18 @@ class FrenchDeck:
         return self._cards[position]
 
     def shuffle(self):
-    	random.shuffle(self._cards)
+        random.shuffle(self._cards)
 
     def print_deck(self):
-    	for card in self._cards:
-    		print(card.rank, card.suit)
+        for card in self._cards:
+            print(card.rank, card.suit)
 
 
-if __name__ == '__main__':
-	theDeck = FrenchDeck()
-	print('unshuffled')
-	theDeck.print_deck()
-#	print(choice(theDeck).rank)
-	theDeck.shuffle()
-	print('shuffled')
-	theDeck.print_deck()
+if __name__ == "__main__":
+    theDeck = FrenchDeck()
+    print("unshuffled")
+    theDeck.print_deck()
+    # 	print(choice(theDeck).rank)
+    theDeck.shuffle()
+    print("shuffled")
+    theDeck.print_deck()
