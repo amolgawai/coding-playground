63031e12dfd8b2e83f6a7e604a46b839ae9e7995
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2021-03-24 12:30:01 +0100

    completed guessing game

--- rust/GuessingGame/src/main.rs
+++ rust/GuessingGame/src/main.rs
@@ -1,3 +1,32 @@
+// Guessing Game
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+	let secret_num = rand::thread_rng().gen_range(1..101);
+
+	loop {
+		println!("Please enter your guess");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess)
+			.expect("Couldn't read the guess");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Please enter a number");
+				continue;
+			},
+		};
+		match guess.cmp(&secret_num) {
+			Ordering::Less => println!("Too Low"),
+			Ordering::Greater => println!("Too High"),
+			Ordering::Equal => {
+				println!("Bingo!!, you win");
+				break;
+			}
+		}
+	}
 }
