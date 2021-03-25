345a6efe8f8ec10f611ac97a1250cf280bedc695
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2021-03-25 13:56:43 +0100

    changed crate names according to rust naming conventions

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+// Guessing Game
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_num = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please enter your guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Couldn't read the guess");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too Low"),
+            Ordering::Greater => println!("Too High"),
+            Ordering::Equal => {
+                println!("Bingo!!, you win");
+                break;
+            }
+        }
+    }
+}
