545908f5954c71cfe2e1cd31f11343f07a13864a
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2022-06-21 18:21:09 +0200

    added further testing and respective source

--- cplusplus/quick_starts/unit_test_frameworks/const_string_lib/include/const_string.h
+++ cplusplus/quick_starts/unit_test_frameworks/const_string_lib/include/const_string.h
@@ -4,18 +4,26 @@
 #include <string>
 
 class const_string {
-public:
-    // Constructors
-    const_string() {}
-    const_string( std::string const& s ) {}
-    const_string( char const* s ) {}
-    // const_string( char const* s, size_t length );
-    // const_string( char const* begin, char const* end );
+  public:
+  // Constructors
+  const_string() : m_begin(""), m_end(m_begin) {}
+  const_string(std::string const& s) : m_begin(s.c_str()), m_end(m_begin + s.length()) {}
+  const_string(char const* s)
+      : m_begin(s ? s : ""), m_end(s ? m_begin + std::strlen(s) : m_begin) {}
+  const_string(char const* first, char const* last) : m_begin(first), m_end(last) {}
+  const_string(char const* s, size_t length);
+  // const_string( char const* begin, char const* end );
 
-    // Access methods
-    // char const* data() const;
-    size_t      length() const;
-    bool        is_empty() const;
+  // Access methods
+  char const* data() const;
+  size_t length() const;
+  bool is_empty() const;
+  void erase();
+
+  private:
+  // Data members
+  char const* m_begin;
+  char const* m_end;
 };
 
 #endif
--- cplusplus/quick_starts/unit_test_frameworks/const_string_lib/src/const_string.cpp
+++ cplusplus/quick_starts/unit_test_frameworks/const_string_lib/src/const_string.cpp
@@ -1,6 +1,13 @@
 #include "const_string.h"
 
+const_string::const_string(char const* s, size_t length) : m_begin(s), m_end(m_begin + length) {
+  if (length == 0) erase();
+}
 
-size_t const_string::length() const { return 5; }
+size_t const_string::length() const { return m_end - m_begin; }
 
-bool const_string::is_empty() const {return true;}
+bool const_string::is_empty() const { return m_end == m_begin; }
+
+void const_string::erase() { m_begin = m_end = ""; }
+
+char const* const_string::data() const { return m_begin; }
