8f453e14315026fe76399c69502bb956d79b9305
Amol Gawai <5114143+amolgawai@users.noreply.github.com>
2022-06-26 10:20:08 +0200

    added braced initialization in place of functional

--- cplusplus/quick_starts/unit_test_frameworks/const_string_lib/tests/boost_tests/src/const_string_tests.cpp
+++ cplusplus/quick_starts/unit_test_frameworks/const_string_lib/tests/boost_tests/src/const_string_tests.cpp
@@ -4,29 +4,29 @@
 #include "const_string.h"
 
 BOOST_AUTO_TEST_CASE(constructors_test) {
-  const_string cs0("");  // 1 //
+  const_string cs0{ "" };  // 1 //
   BOOST_CHECK_EQUAL(cs0.length(), (size_t)0);
   BOOST_CHECK(cs0.is_empty());
 
-  const_string cs01(NULL);  // 2 //
+  const_string cs01{ NULL };  // 2 //
   BOOST_CHECK_EQUAL(cs01.length(), (size_t)0);
   BOOST_CHECK(cs01.is_empty());
 
-  const_string cs1("test_string");  // 3 //
+  const_string cs1{ "test_string" };  // 3 //
   BOOST_CHECK_EQUAL(std::strcmp(cs1.data(), "test_string"), 0);
   BOOST_CHECK_EQUAL(cs1.length(), std::strlen("test_string"));
 
-  std::string s("test_string");  // 4 //
+  std::string s{ "test_string" };  // 4 //
   const_string cs2(s);
   BOOST_CHECK_EQUAL(std::strcmp(cs2.data(), "test_string"), 0);
 
-  const_string cs3(cs1);  // 5 //
+  const_string cs3{ cs1 };  // 5 //
   BOOST_CHECK_EQUAL(std::strcmp(cs1.data(), "test_string"), 0);
 
-  const_string cs4("test_string", 4);  // 6 //
+  const_string cs4{ "test_string", 4 };  // 6 //
   BOOST_CHECK_EQUAL(std::strncmp(cs4.data(), "test", cs4.length()), 0);
 
-  const_string cs5(s.data(), s.data() + s.length());  // 7 //
+  const_string cs5{ s.data(), s.data() + s.length() };  // 7 //
   BOOST_CHECK_EQUAL(std::strncmp(cs5.data(), "test_string", cs5.length()), 0);
 
   // const_string cs_array[] = {"str1", "str2"};  // 8 //
