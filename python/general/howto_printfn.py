1a4432e91f3ebe05d76a5fc8cb9f2d32eeb595cd
Amol <5114143+amolgawai@users.noreply.github.com>
2022-07-28 20:17:38 +0000

    Formatted according to the formatting rules

--- python/general/howto_printfn.py
+++ python/general/howto_printfn.py
@@ -8,33 +8,36 @@ import math
 import time
 
 
-def printSeparator():
-    print(70 * "=")
+def print_separator(sep_char="="):
+    """Prints a line 70 chars wide"""
+    print(70 * sep_char)
 
 
 print(__doc__)
-printSeparator()
+print_separator()
 print("escaping with backslash")
 print("new lines\nand\n\ttabs too")
 print("Octal 275 is half: \275")
 print("hex 0xBD is Octal 275: \xBD")
-printSeparator()
+print_separator()
 print("string formatting")
 # c-style
-name = "James"
-sirname = "Bond"
-age = 34
-print("Hello Mr. %s %s" % (name, sirname))
-print("Mr bond is %d years old" % age)
+NAME = "James"
+SURNAME = "Bond"
+AGE = 34
+print("Hello Mr. %s %s" % (NAME, SURNAME))
+print("Mr bond is %d years old" % AGE)
 print("pi: %f \nshort pi %0.2f" % (math.pi, math.pi))
 # format with python 3.0
 print("My name is {1}, {0} {1}".format("James", "Bond"))
 # f-string - python 3.6
-print(f"I am {age} years old")
+print(f"I am {AGE} years old")
 print(f"f-string support arithmetic as in 5*pi is {math.pi*5}")
-print(f"f-string also support formatting as in 5*pi till two digits is {math.pi*5:.2f}")
+print(
+    f"f-string also support formatting as in 5*pi till two digits is {math.pi*5:.2f}"
+)
 
-printSeparator()
+print_separator()
 # sep, flush
 print(
     "different separator in print instead of default ' '",
@@ -43,14 +46,21 @@ print(
     "e.g.",
     sep="\n",
 )
-data = [["year", "last", "first"], [1943, "Cat", "Catty"], [1945, "Dog", "Dogbert"]]
+data = [
+    ["year", "last", "first"],
+    [1943, "Cat", "Catty"],
+    [1945, "Dog", "Dogbert"],
+]
 for row in data:
     print(*row, sep=",")
 # end, flush
 def count_items(items):
-    print("Counting", end="", flush=True)  # don't add \n but still flush the print
+    """Counts items in the list"""
+    print(
+        "Counting", end="", flush=True
+    )  # don't add \n but still flush the print
     num = 0
-    for item in items:
+    for _ in items:
         num += 1
         time.sleep(1)
         print(".", end="", flush=True)
@@ -58,4 +68,4 @@ def count_items(items):
 
 
 count_items(data)
-printSeparator()
+print_separator()
